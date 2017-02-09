cd0b228f1ed98b161c234a0556358af18a93aad9
Lucas BILLET <Lucas BILLET>
2017-02-09 11:49:56 +0100

    Exercise 3

--- src/product.js
+++ src/product.js
@@ -1,38 +1,71 @@
 var chalk = require('chalk');
 
-function Product(name, price) {
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+    toString(currency, rateCurrency) {
+        var currency = currency || '$';
+        var rateCurrency = rateCurrency || 1;
+        return `${chalk.yellow(this.name)} cost ${chalk.red(this.price * rateCurrency)}${currency}`;
+    }
+};
+
+/*function Product(name, price) {
   this.name = name;
   this.price = price;
-  this.toString = function(currency, rateCurrency) {
+  this.toString = (currency, rateCurrency) => {
     var currency = currency || '$';
     var rateCurrency = rateCurrency || 1;
-    return chalk.yellow(this.name) + ' cost ' + chalk.red(this.price * rateCurrency) + currency;
+    return `${chalk.yellow(this.name)} cost ${chalk.red(this.price * rateCurrency)}${currency}`;
   }
+}*/
+
+class Book extends Product {
+      constructor(name, price, isbn) {
+        super(name, price);
+        this.isbn = isbn;
+      }
 }
 
-function Book(name, price, isbn) {
+/*function Book(name, price, isbn) {
   Product.apply(this, [name, price]);
   this.isbn = isbn;
 }
 Book.prototype = Object.create(Product.prototype, {
   constructor: {value: Book}
-});
+});*/
 
-function DVD(name, price, moovie) {
+class DVD extends Product {
+      constructor(name, price, moovie) {
+        super(name, price);
+        this.moovie = moovie;
+      }
+}
+
+/*function DVD(name, price, moovie) {
   Product.apply(this, [name, price]);
   this.moovie = moovie;
 }
 DVD.prototype = Object.create(Product.prototype, {
   constructor: {value: DVD}
-});
+});*/
 
-function VideoGame(name, price, platform) {
+class VideoGame extends Product {
+      constructor(name, price, platform) {
+        super(name, price);
+        this.platform = platform;
+      }
+}
+
+/*function VideoGame(name, price, platform) {
   Product.apply(this, [name, price]);
   this.platform = platform;
 }
 VideoGame.prototype = Object.create(Product.prototype, {
   constructor: {value: VideoGame}
-});
+});*/
 
 module.exports = {
   Book: Book,
